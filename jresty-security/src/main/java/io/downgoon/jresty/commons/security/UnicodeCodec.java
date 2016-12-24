441db9f781e3ae6dd962c2bd1814f2878c4f7111
downgoon <downgoon@qq.com>
2016-12-24 21:13:06 +0800

    add unicode encoding

---
+++ jresty-security/src/main/java/io/downgoon/jresty/commons/security/UnicodeCodec.java
@@ -0,0 +1,22 @@
+package io.downgoon.jresty.commons.security;
+
+
+public class UnicodeCodec {
+
+	public static String encode(final String plain) {
+		StringBuffer r = new StringBuffer();
+		int i = 0;
+		while(i<plain.length()) {
+			String hexCode = Integer.toHexString(plain.codePointAt(i)).toUpperCase();
+		    String hexCodeWithAllLeadingZeros = "0000" + hexCode;
+		    String hexCodeWithLeadingZeros = hexCodeWithAllLeadingZeros.substring(hexCodeWithAllLeadingZeros.length()-4);
+		    r.append("\\u" + hexCodeWithLeadingZeros);
+		    i++;
+		}
+		return r.toString();
+	}
+
+	public static void main(String[] args) {
+		System.out.println(UnicodeCodec.encode("张柏芝"));;
+	}
+}
